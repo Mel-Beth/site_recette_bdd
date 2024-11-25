5a6c4dc9a83df0f361b47bd1ab98b0575158a024
Mélanie Bethermat <melaniebethermat@gmail.com>
2024-11-25 13:42:18 +0100

    reajout a la main des dossiers et fichiers nav/footer

--- index.php
+++ index.php
@@ -1,21 +1,25 @@
-<?php 
+<?php
+
 require("assets/data/db.php");
+
 ?>
+
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Site de recettes</title>
-    <link rel="stylesheet" href="assets/css/style.css">
-</head>
+
+<?php require("includes/head.html"); ?>
+
 <body>
-    
+
+    <?php require("includes/header.html"); ?>
 
 
 
 
+    <?php require("includes/footer.html"); ?>
 
     <script src="assets/js/script.js"></script>
+
 </body>
+
 </html>
